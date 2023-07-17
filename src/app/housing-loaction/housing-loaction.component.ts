a601a7789be76c54c3edb189fb1771e3275173b8
Utsav Basyal <utsabbasyal12@gmail.com>
2023-07-17 12:04:38 +0545

    added home components data

--- src/app/housing-loaction/housing-loaction.component.ts
+++ src/app/housing-loaction/housing-loaction.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-loaction',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      housing-loaction works!
-    </p>
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exteriro photo of {{ housingLocation.name }}"
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}</p>
+    </section>
   `,
-  styleUrls: ['./housing-loaction.component.css']
+  styleUrls: ['./housing-loaction.component.css'],
 })
 export class HousingLoactionComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
